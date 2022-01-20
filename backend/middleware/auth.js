74d8f0f2d5213c5a347b522a2aeb93df08cc0ce1
madalina <madalina.florea@tmtanalysis.com>
2022-01-20 17:11:56 +0200

    first commit

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+// cand utilizatorul vrea sa stearga/editeze/ posteze review => auth middleware => care confirma daca are permisiunea (next())
+const auth = async ( req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        console.log("auth " + token)
+        let decodedToken;
+        if ( token) {
+            decodedToken = jwt.verify(token,"appptmaster");
+            req.username = decodedToken?.username;
+        }
+        next();
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default auth;
